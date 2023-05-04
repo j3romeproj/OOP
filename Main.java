2444fb4a23d204a878e7fa8d730c80af8a204bc1
j3romeproj <111049617+j3romeproj@users.noreply.github.com>
2023-05-05 07:40:58 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declaration of text
+        String text = "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.";
+        // Printing the text in to lower case using .toLowerCase
+        System.out.println(text.toLowerCase());
+    }
+}
