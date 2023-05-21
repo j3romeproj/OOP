7d06a5f809ebbe4c2c03aef11dad3066dd7cd685
jvsubijano <132254268+jvsubijano@users.noreply.github.com>
2023-05-21 20:38:29 +0800

    Update InventoryManagementSystem.java

--- Case Study/InventoryManagementSystem.java
+++ Case Study/InventoryManagementSystem.java
@@ -8,7 +8,7 @@ public class Main {
     }
 }
 // Inventory Management Class for the user interface(console)
-InventoryManagement {
+class InventoryManagement {
     public void interfaced() {
         Inventory inventory = new Inventory();
         Scanner scanner = new Scanner(System.in);
